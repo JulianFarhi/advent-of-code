c4ff328b033d92df6ecb43fda9e74116ef1910d3
julian.farhi1 <julian.farhi@implicity.fr>
2021-12-05 11:08:17 +0100

    Added advent of code 2021

---
+++ 2021/1/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const readLine = require('readline')
+
+async function process() {
+    const stream = fs.createReadStream('data.text');
+    const reader = readLine.createInterface({
+        input: stream,
+        crlfDelay: Infinity
+    })
+    let total = 0;
+    let index = 0;
+    let previousParsedLine = null;
+    for await (const line of reader) {
+        const currentParsedLine = parseInt(line);
+        if(previousParsedLine) {
+            total += currentParsedLine > previousParsedLine ? 1 : 0
+        }
+        index++
+        previousParsedLine = currentParsedLine;
+    }
+    console.log('total :', total);
+}
+
+process()
+
